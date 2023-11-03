08ab46f891f6f0660024a90497d56038ff48f88f
Umeen Rathnayake <umeenbg16@gmail.com>
2023-11-03 18:03:59 +0530

    signup adjustments

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -120,7 +120,7 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/login" variant="body2">
+                <Link href="/" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
